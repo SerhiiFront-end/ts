ec12d95e8da1617674d95bc8582de66442a9c3b9
SerhiiFront-end <aorl9048@gmail.com>
2023-03-05 03:23:39 +0000

    OurTeam component

---
+++ src/components/UI/OurTeam.tsx
@@ -0,0 +1,13 @@
+import Person from "./Persons/PersonDetails"
+export default function OurTeam() {
+    const persons = [
+        {name: 'Altynay Guney', date: "From 2013"},
+        {name: 'Graham Peak', date: "From 2022"},
+        {name: 'Jelena Pazaksina', date: 'April 2019- December 2022'}
+      ]
+  return (
+    <div className="flex justify-between w-3/4 m-auto">
+        {persons.map(el => <Person name={el.name} date={el.date}/>)}    
+    </div>
+  )
+}
--- src/components/pages/AboutPage/AboutPage.tsx
+++ src/components/pages/AboutPage/AboutPage.tsx
@@ -1,9 +1,10 @@
-import Person from '../../UI/Persons/PersonDetails'
+import OurTeam from "../../UI/OurTeam"
 export default function AboutPage() {
+  
   return (
     <div>
         <h1 className='text-center text-slate-50 text-3xl mt-5'>Meet The Team</h1>
-        <Person name='Altynay Guney' date='From 2013'/>
+        <OurTeam/>
     </div>
   )
 }
