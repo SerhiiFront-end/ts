36b53e794fcd9d06401b57b287bd0dda1c8bd6c9
SerhiiFront-end <aorl9048@gmail.com>
2023-04-08 23:48:52 +0100

    correct links

--- src/components/UI/Footer/Other/SocialMedia.tsx
+++ src/components/UI/Footer/Other/SocialMedia.tsx
@@ -1,13 +1,16 @@
 export default function SocialMedia() {
 	return (
 		<div className='flex'>
-			<a className='w-10' href='https://www.instagram.com/cosmocic/'>
+			<a className='w-10 h-10' href='https://www.instagram.com/cosmocic/'>
 				<img
 					src='https://www.instagram.com/static/images/ico/favicon-192.png/68d99ba29cc8.png'
 					alt='Instagram Logo'
 				/>
 			</a>
-			<a className='w-10' href='https://www.facebook.com/Cosmocic/'>
+			<a
+				className='w-10 h-10 ml-5'
+				href='https://www.facebook.com/Cosmocic/'
+			>
 				<img
 					src='https://www.facebook.com/images/fb_icon_325x325.png'
 					alt='Facebook Logo'
